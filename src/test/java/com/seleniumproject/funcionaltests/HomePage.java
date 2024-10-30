95e9b96894d54db9304ce4e867617b420fc9fc46
Patryk Prentki <patryk.prentki90@gmail.com>
2024-10-30 23:04:10 +0100

    nintendoCheck method

--- src/test/java/com/seleniumproject/funcionaltests/HomePage.java
+++ src/test/java/com/seleniumproject/funcionaltests/HomePage.java
@@ -5,6 +5,7 @@ import com.seleniumproject.webBase.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,7 +15,6 @@ import java.util.Set;
 public class HomePage extends BasePage {
 
 
-
     @Test
     public void homePageTest() {
         String pageTitle = driver.getTitle();
@@ -22,9 +22,9 @@ public class HomePage extends BasePage {
         Set<Cookie> cookies = driver.manage().getCookies();
 
 
-        Assert.assertEquals(pageTitle,"Your Store");
-        Assert.assertEquals(currentUrl,"http://localhost/ProjectOpenShop/");
-        Assert.assertEquals(cookies.size(),2);
+        Assert.assertEquals(pageTitle, "Your Store");
+        Assert.assertEquals(currentUrl, "http://localhost/ProjectOpenShop/");
+        Assert.assertEquals(cookies.size(), 2);
     }
 
     @Test
@@ -35,9 +35,8 @@ public class HomePage extends BasePage {
         WebElement tv = driver.findElement(By.xpath("//a[contains(text(), 'Apple')]"));
 
 
-
         Assert.assertEquals(pageTitle, "Search - Apple");
-        Assert.assertEquals(tv.getText(),"Apple Cinema 30\"");
+        Assert.assertEquals(tv.getText(), "Apple Cinema 30\"");
     }
 
     @Test
@@ -45,23 +44,38 @@ public class HomePage extends BasePage {
         driver.findElement(By.xpath("//span[text()='Currency']")).click();
         int ss = driver.findElements(By.xpath("//ul[@class='dropdown-menu show']//li")).size();
         List<WebElement> elements = driver.findElements(By.xpath("//ul[@class='dropdown-menu show']//li"));
-        for(WebElement el : elements) {
-            if(el.getText().equals("£ Pound Sterling")){
+        for (WebElement el : elements) {
+            if (el.getText().equals("£ Pound Sterling")) {
                 el.click();
                 break;
             }
         }
         String price = driver.findElement(By.xpath("//button[contains(text(), '£0.00')]")).getText();
 
-        Assert.assertEquals(ss,3);
-        Assert.assertEquals(price,"0 item(s) - £0.00");
+        Assert.assertEquals(ss, 3);
+        Assert.assertEquals(price, "0 item(s) - £0.00");
 
     }
 
     @Test
     public void nintendoCheck() {
-    int vv = driver.findElements(By.xpath("//div[@class='col-2 text-center']//img")).size();
-        System.out.println(vv);
+        int howManyLogos = driver.findElements(By.xpath("//div[@class='col-2 text-center']//img")).size();
+        List<WebElement> logos = driver.findElements(By.xpath("//div[@id='carousel-banner-1']//div[@class='carousel-inner']//img"));
+        boolean logoIsDisplayed = false;
+            for(WebElement log : logos) {
+                String name2 = "Nintendo";
+                if(log.getAttribute("alt").equals(name2)!= log.isDisplayed()){
+                    Actions actions = new Actions(driver);
+                    WebElement arrow = driver.findElement(By.xpath("//button[@data-bs-target='#carousel-banner-1']//span[@class='fa-solid fa-chevron-right']"));
+                    actions.click(arrow).perform();
+                } else {
+                    logoIsDisplayed = true;
+                    break;
+                }
+            }
+        Assert.assertEquals(howManyLogos,11);
+        Assert.assertTrue(logoIsDisplayed,"Logo is displayed");
+        }
 
     }
-}
+
