5b7d57d574fc9bdef324de993d0047ecbea51b6d
Patryk Prentki <patryk.prentki90@gmail.com>
2024-11-01 14:46:09 +0100

    added one test loginWithInvalidCredentials

--- src/test/java/com/seleniumproject/unhappypaths/unhappyLogins.java
+++ src/test/java/com/seleniumproject/unhappypaths/unhappyLogins.java
@@ -53,4 +53,19 @@ public class unhappyLogins extends BasePage {
 
         Assert.assertEquals(elementAlert.getText(),"Warning: No match for E-Mail Address and/or Password.");
     }
+
+    @Test
+    public void loginWithInvalidCredentials() {
+        driver.findElement(By.xpath("//span[text()='My Account']//following-sibling::i")).click();
+        driver.findElement(By.xpath("//ul[@class='dropdown-menu dropdown-menu-right show']//a[text()='Login']")).click();
+        driver.findElement(By.id("input-email")).sendKeys("test@gmail.com");
+        driver.findElement(By.id("input-password")).sendKeys("test666");
+        driver.findElement(By.xpath("//button[text()='Login']")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//i[@class='fa-solid fa-circle-exclamation']")));
+        WebElement elementAlert = driver.findElement(By.xpath("//dirv[@class='alert alert-danger alert-dismissible']"));
+
+        Assert.assertEquals(elementAlert.getText(),"Warning: No match for E-Mail Address and/or Password.");
+    }
 }
