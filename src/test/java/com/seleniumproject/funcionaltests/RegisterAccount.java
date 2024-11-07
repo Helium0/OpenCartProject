638a87c904c5a1c1abf197707d200c87660b6fdc
Patryk Prentki <patryk.prentki90@gmail.com>
2024-11-07 21:57:58 +0100

    Modified all test in to POM model

--- src/test/java/com/seleniumproject/funcionaltests/RegisterAccount.java
+++ src/test/java/com/seleniumproject/funcionaltests/RegisterAccount.java
@@ -1,194 +1,18 @@
 package com.seleniumproject.funcionaltests;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import com.seleniumproject.pages.RegisterAccountPage;
 import com.seleniumproject.webBase.BasePage;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 
 public class RegisterAccount extends BasePage {
 
     @Test
     public void moveToRegisterAccountTest() {
-        driver.findElement(By.xpath("//span[text()='My Account']//following-sibling::i")).click();
-        driver.findElement(By.xpath("//ul[@class='dropdown-menu dropdown-menu-right show']//a[text()='Register']")).click();
+        RegisterAccountPage registerAccountPage = new RegisterAccountPage(driver);
+        registerAccountPage.setMyAccount();
+        registerAccountPage.setRegisterAccount();
         String title = driver.getTitle();
 
         Assert.assertEquals(title,"Register Account");
@@ -202,59 +26,42 @@ public class RegisterAccount extends BasePage {
         registerAccountPage.setFirstName();
         registerAccountPage.setLastName();
         registerAccountPage.setUserEmail();
-
-
-
         registerAccountPage.setUserPassword();
         registerAccountPage.setRegisterButton();
 
-        Assert.assertEquals(registerAccountPage.getFirstName(),"First Name");
+        Assert.assertEquals(registerAccountPage.getFirstNameAttribute(),"First Name");
         Assert.assertTrue(registerAccountPage.getRegisterButton().isEnabled());
     }
 
     @Test
     public void registerAccountWithoutData() {
-        driver.findElement(By.xpath("//span[text()='My Account']//following-sibling::i")).click();
-        driver.findElement(By.xpath("//ul[@class='dropdown-menu dropdown-menu-right show']//a[text()='Register']")).click();
-        driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
+        RegisterAccountPage registerAccountPage = new RegisterAccountPage(driver);
+        registerAccountPage.setMyAccount();
+        registerAccountPage.setRegisterAccount();
+        registerAccountPage.setContinueButton();
+        registerAccountPage.waitMethod(registerAccountPage.fullElementList());
 
-        WebElement errorFirstName = driver.findElement(By.id("error-firstname"));
-        WebElement errorLastName = driver.findElement(By.id("error-lastname"));
-        WebElement errorEmail = driver.findElement(By.id("error-email"));
-        WebElement errorPassword = driver.findElement(By.id("error-password"));
-        List<WebElement> lista = new ArrayList<>();
-        lista.add(errorFirstName);
-        lista.add(errorLastName);
-        lista.add(errorEmail);
-        lista.add(errorPassword);
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
-        wait.until(ExpectedConditions.visibilityOfAllElements(lista));
-
-        Assert.assertEquals(errorFirstName.getText(), "First Name must be between 1 and 32 characters!");
-        Assert.assertEquals(errorLastName.getText(), "Last Name must be between 1 and 32 characters!");
-        Assert.assertEquals(errorEmail.getText(), "E-Mail Address does not appear to be valid!");
-        Assert.assertEquals(errorPassword.getText(), "Password must be between 4 and 20 characters!");
+        Assert.assertEquals(registerAccountPage.getErrorFirstName(), "First Name must be between 1 and 32 characters!");
+        Assert.assertEquals(registerAccountPage.getErrorLastName(), "Last Name must be between 1 and 32 characters!");
+        Assert.assertEquals(registerAccountPage.getErrorUserEmail(), "E-Mail Address does not appear to be valid!");
+        Assert.assertEquals(registerAccountPage.getErrorUserPassword(), "Password must be between 4 and 20 characters!");
 
     }
 
     @Test
     public void registerAccountWithoutPrivacyPolicy() {
-        driver.findElement(By.xpath("//span[text()='My Account']//following-sibling::i")).click();
-        driver.findElement(By.xpath("//ul[@class='dropdown-menu dropdown-menu-right show']//a[text()='Register']")).click();
-        String firstName = driver.findElement(By.cssSelector("#input-firstname")).getAttribute("placeholder");
-        driver.findElement(By.cssSelector("#input-firstname")).sendKeys("Patryk");
-        driver.findElement(By.cssSelector("#input-lastname")).sendKeys("Tester");
-        driver.findElement(By.cssSelector("#input-email")).sendKeys("test666@gmail.com");
+        RegisterAccountPage registerAccountPage = new RegisterAccountPage(driver);
+        registerAccountPage.setMyAccount();
+        registerAccountPage.setRegisterAccount();
+        registerAccountPage.setFirstName();
+        registerAccountPage.setLastName();
+        registerAccountPage.setUserEmail();
+        registerAccountPage.setUserPassword();
+        registerAccountPage.setContinueButton();
+        registerAccountPage.waitMethodForWebelement();
 
-        driver.findElement(By.cssSelector("#input-password")).sendKeys("test");
-        driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
-
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5000));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//i[@class='fa-solid fa-circle-exclamation']")));
-        WebElement alert = driver.findElement(By.xpath("//dirv[@class='alert alert-danger alert-dismissible']"));
-
-        Assert.assertTrue(alert.isDisplayed());
-        Assert.assertEquals(alert.getText(),"Warning: You must agree to the Privacy Policy!");
+        Assert.assertTrue(registerAccountPage.getAlert().isDisplayed());
+        Assert.assertEquals(registerAccountPage.getAlert().getText(),"Warning: You must agree to the Privacy Policy!");
 
     }
 }
