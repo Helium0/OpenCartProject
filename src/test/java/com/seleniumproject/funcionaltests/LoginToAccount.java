b41899be84f2ee7b3c0d53405754cf24b52d2033
Patryk Prentki <patryk.prentki90@gmail.com>
2024-11-11 20:59:15 +0100

    Refactored class, moved in to different directory

--- src/test/java/com/seleniumproject/funcionaltests/LoginToAccount.java
+++ src/test/java/com/seleniumproject/funcionaltests/LoginToAccount.java
@@ -1,13 +1,21 @@
-package com.seleniumproject.unhappypaths;
+package com.seleniumproject.funcionaltests;
 
 import com.seleniumproject.pages.LoginAccountPage;
 import com.seleniumproject.pages.RegisterAccountPage;
 import com.seleniumproject.webBase.BasePage;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class UnhappyLogins extends BasePage {
+
+public class LoginToAccount extends BasePage {
 
     @Test
     public void loginWithoutCredentials() {
@@ -61,5 +69,14 @@ public class UnhappyLogins extends BasePage {
 
         Assert.assertEquals(loginAccountPage.getLoginAlert().getText(), "Warning: No match for E-Mail Address and/or Password.");
 
+    }
+
+    @Test(dataProvider = "dp")
+    public void loginsFromJSON(String data) throws IOException, ParseException {
+        JSONParser jsonParser = new JSONParser();
+        FileReader file = new FileReader(".\\src\\test\\resources\\login.json");
+        Object obj = jsonParser.parse(file);
+        JSONObject jsonObject = (JSONObject) obj;
+        JSONArray jsonArray = (JSONArray) jsonObject.get("userLogins");
     }
 }
