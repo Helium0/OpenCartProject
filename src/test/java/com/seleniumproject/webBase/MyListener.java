5dcc2f93a5e360581d56efd364d22b4f3f1b6b23
Patryk Prentki <patryk.prentki90@gmail.com>
2024-11-14 23:30:39 +0100

    MyListener class with method when some test fail it will takes screenshoot

---
+++ src/test/java/com/seleniumproject/webBase/MyListener.java
@@ -0,0 +1,30 @@
+package com.seleniumproject.webBase;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
+
+
+public class MyListener extends BasePage implements ITestListener {
+
+
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+
+        ITestListener.super.onTestFailure(result);
+        TakesScreenshot screen = (TakesScreenshot) driver;
+        File fileU = screen.getScreenshotAs(OutputType.FILE);
+        int number = (int)(Math.random()*1000);
+        try {
+            FileUtils.copyFile(fileU,new File(".\\src\\test\\resources\\screenshoots\\fileName"+number+".png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
