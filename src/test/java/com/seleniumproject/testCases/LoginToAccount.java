b20ca226b5ca664e846ade99e4033a2629c97a3a
Patryk Prentki <patryk.prentki90@gmail.com>
2024-11-17 23:49:59 +0100

    Moved in to different directory

--- src/test/java/com/seleniumproject/testCases/LoginToAccount.java
+++ src/test/java/com/seleniumproject/testCases/LoginToAccount.java
@@ -1,24 +1,13 @@
-package com.seleniumproject.funcionaltests;
+package com.seleniumproject.testCases;
 
 import com.seleniumproject.pages.LoginAccountPage;
 import com.seleniumproject.pages.RegisterAccountPage;
 import com.seleniumproject.utilities.DataProviders;
 import com.seleniumproject.webBase.BasePage;
 import com.seleniumproject.webBase.MyListener;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 
 @Listeners(value = {MyListener.class})
 public class LoginToAccount extends BasePage {
