2a50fdd5f97b5e3ba3dc373e481574c68e887b5d
Patryk Prentki <patryk.prentki90@gmail.com>
2024-12-04 23:34:12 +0100

    Created SeleniumHelper class where I will keep reusable methods for all classes

---
+++ src/test/java/com/seleniumproject/webBase/SeleniumHelper.java
@@ -0,0 +1,32 @@
+package com.seleniumproject.webBase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.Duration;
+
+public class SeleniumHelper  {
+
+    private static WebDriver driver;
+
+
+
+
+
+
+    public static WebDriverWait waitObjMethod() {
+        return new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+    public static Statement connectionStatement() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/myshop", "root","admin");
+        return connection.createStatement();
+    }
+
+
+
+}
