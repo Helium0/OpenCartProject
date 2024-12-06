a57fa19c4eccf880d56b3c423c46acef43d45c50
Patryk Prentki <patryk.prentki90@gmail.com>
2024-12-06 23:26:15 +0100

    In BaseComponentPage class I pass main locator to the ProductComponent class

---
+++ src/test/java/com/seleniumproject/webBase/ProductComponent.java
@@ -0,0 +1,27 @@
+package com.seleniumproject.webBase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.function.Predicate;
+
+public class ProductComponent extends BaseComponentPage {
+
+    private static final By PRODUCT_BY_NAME = By.xpath("//div[@class='description']//a");
+
+    private static final By CURRENT_PRICE_BY = By.xpath("//span[@class='price-new']");
+
+    public ProductComponent(WebElement rootElement) {
+        super(rootElement);
+    }
+
+    public String getName() {
+        return rootElement.findElement(PRODUCT_BY_NAME).getText();
+    }
+
+    public String getPrice() {
+        return rootElement.findElement(CURRENT_PRICE_BY).getText();
+    }
+
+
+}
