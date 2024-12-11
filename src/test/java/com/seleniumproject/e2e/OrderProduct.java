b2fe85dfcee1c621fcf3b56c8d1224a9127f9d21
Patryk Prentki <patryk.prentki90@gmail.com>
2024-12-11 23:14:03 +0100

    Added test from login in to application and succesfully buy a product

--- src/test/java/com/seleniumproject/e2e/OrderProduct.java
+++ src/test/java/com/seleniumproject/e2e/OrderProduct.java
@@ -6,6 +6,8 @@ import com.seleniumproject.pages.RegisterAccountPage;
 import com.seleniumproject.webBase.BasePage;
 import com.seleniumproject.webBase.ReadProperties;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 import java.io.IOException;
 
@@ -32,6 +34,19 @@ public class OrderProduct extends BasePage {
         cartPage.clickOnItemsCart();
         driver.findElement(By.xpath("//strong[text()=' View Cart']")).click();
         driver.findElement(By.xpath("//a[text()='Checkout']")).click();
+        Thread.sleep(4000);
+        WebElement ship = driver.findElement(By.id("input-shipping-address"));
+        Select shipSel = new Select(ship);
+        shipSel.selectByValue("1");
+//        driver.findElement(By.id("button-shipping-address")).click();
+        driver.findElement(By.id("button-shipping-methods")).click();
+        driver.findElement(By.id("input-shipping-method-flat-flat")).click();
+        driver.findElement(By.id("button-shipping-method")).click();
+        driver.findElement(By.id("button-payment-methods")).click();
+        driver.findElement(By.id("input-payment-method-cod-cod")).click();
+        driver.findElement(By.id("button-payment-method")).click();
+        driver.findElement(By.id("button-confirm")).click();
+
         Thread.sleep(4000);
     }
 
